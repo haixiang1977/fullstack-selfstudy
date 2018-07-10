aa9eb625517fcf4e0ab34a2dca45e4d67d6ccf30
bcxa <bcxa.sz@gmail.com>
2018-07-10 14:10:05 +0000

    add backend router to answer 'GET' response
    Signed-off-by: bcxa <bcxa.sz@gmail.com>

--- contactlist/server.js
+++ contactlist/server.js
@@ -2,5 +2,16 @@ var express = require('express');
 var app = express();
 // setup static direcory, client can access resource without router
 app.use(express.static(__dirname + "/public"));
+app.get('/contactList', function(req, res) {
+    console.log("i received a GET request");
+    person1 = {
+        name: 'liu',
+        email: 'haixiang.liu@gmail.com',
+        number: '11111111'
+    };
+    var contactList = [person1];
+    res.json(contactList);
+})
+
 app.listen(3000);
 console.log("server running on port 3000");
