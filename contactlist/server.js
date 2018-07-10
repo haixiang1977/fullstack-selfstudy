964699103600a46425d95db62d41a9b6b07af21e
bcxa <bcxa.sz@gmail.com>
2018-07-10 13:20:16 +0000

    add index.html title with bootstrap css
    Signed-off-by: bcxa <bcxa.sz@gmail.com>

--- contactlist/server.js
+++ contactlist/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+// setup static direcory, client can access resource without router
 app.use(express.static(__dirname + "/public"));
 app.listen(3000);
 console.log("server running on port 3000");
